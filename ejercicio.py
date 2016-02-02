7b3201c27fa2a12607ff1a75db9bae7fa1158857
bbeloqui <bbeloqui@hotmail.com>
2016-02-02 13:40:16 +0100

    ejercicio1

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
+fd = open("/etc/passwd","r")
+lineas = fd.readlines()
+for  linea  in lineas:                   
+	print "Nombre: " + linea.split(":")[0] + " ,shell: " + linea.split(":")[-1]
+	 
+	
